92d76cf795ddc321e7b98506271588352c8de5d7
Nolan Pham <43715108+TuanAnh-Pham@users.noreply.github.com>
2020-06-13 17:57:15 +0700

    upload file

---
+++ app.py
@@ -0,0 +1,51 @@
+from __future__ import unicode_literals
+from flask import Flask,render_template,url_for,request
+
+from summary_vn import summarizer
+from gensim.summarization import summarize
+
+import time
+import spacy
+nlp = spacy.load('en')
+app = Flask(__name__)
+
+@app.route('/about')
+def about():
+	return render_template('index.html')
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+# Reading Time
+def readingTime(mytext):
+	total_words = len([ token.text for token in nlp(mytext)])
+	estimatedTime = total_words/200.0
+	return estimatedTime
+
+@app.route('/analyze_vn',methods=['GET','POST'])
+def analyze():
+	start = time.time()
+	if request.method == 'POST':
+		rawtext = request.form['rawtext']
+		final_reading_time = readingTime(rawtext)
+		final_summary = summarizer(rawtext)
+		summary_reading_time = readingTime(final_summary)
+		end = time.time()
+		final_time = end-start
+	return render_template('index.html',ctext=rawtext,final_summary=final_summary,final_time=final_time,final_reading_time=final_reading_time,summary_reading_time=summary_reading_time)
+
+@app.route('/analyze_en',methods=['GET','POST'])
+def analyze():
+	start = time.time()
+	if request.method == 'POST':
+		rawtext = request.form['rawtext']
+		final_reading_time = readingTime(rawtext)
+		final_summary = summarize(rawtext)
+		summary_reading_time = readingTime(final_summary)
+		end = time.time()
+		final_time = end-start
+	return render_template('index.html',ctext=rawtext,final_summary=final_summary,final_time=final_time,final_reading_time=final_reading_time,summary_reading_time=summary_reading_time)
